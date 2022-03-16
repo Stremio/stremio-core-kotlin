557deb3adb90390897fe406a995fd2636278987a
nklhrstv <me@nikolahristov.com>
2022-03-16 14:14:05 +0200

    use Int for fileIdx

--- src/main/kotlin/com/stremio/core/types/resource/StreamSource.kt
+++ src/main/kotlin/com/stremio/core/types/resource/StreamSource.kt
@@ -3,7 +3,7 @@ package com.stremio.core.types.resource
 sealed class StreamSource {
     data class Url(val url: String) : StreamSource()
     data class YouTube(val ytId: String) : StreamSource()
-    data class Torrent(val infoHash: String, val fileIdx: UInt?, val announce: List<String>) :
+    data class Torrent(val infoHash: String, val fileIdx: Int?, val announce: List<String>) :
         StreamSource()
 
     data class External(val externalUrl: String) : StreamSource()
