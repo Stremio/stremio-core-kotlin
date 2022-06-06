b8ae06a2d5d7fc09946afbcc2ec4ebe8d0930534
TheBeastLT <pauliox@beyond.lt>
2022-06-06 16:16:13 +0300

    use meta preview for add to library action

--- src/main/kotlin/com/stremio/core/runtime/msg/ActionCtx.kt
+++ src/main/kotlin/com/stremio/core/runtime/msg/ActionCtx.kt
@@ -2,13 +2,13 @@ package com.stremio.core.runtime.msg
 
 import com.stremio.core.types.api.AuthRequest
 import com.stremio.core.types.profile.Profile
-import com.stremio.core.types.resource.MetaItem
+import com.stremio.core.types.resource.MetaItemPreview
 
 sealed class ActionCtx {
     data class Authenticate(val args: AuthRequest) : ActionCtx()
     class Logout : ActionCtx()
     data class UpdateSettings(val args: Profile.Settings) : ActionCtx()
-    data class AddToLibrary(val args: MetaItem) : ActionCtx()
+    data class AddToLibrary(val args: MetaItemPreview) : ActionCtx()
     data class RemoveFromLibrary(val args: String) : ActionCtx()
     data class RewindLibraryItem(val args: String) : ActionCtx()
     class PushUserToAPI : ActionCtx()
