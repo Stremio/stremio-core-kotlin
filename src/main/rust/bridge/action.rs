6bc638dae1ecafd16ef8588919a0c4acf7c1f28d
TheBeastLT <pauliox@beyond.lt>
2022-06-07 23:12:30 +0300

    reduce oneof nesting

--- src/main/rust/bridge/action.rs
+++ src/main/rust/bridge/action.rs
@@ -13,24 +13,24 @@ use crate::protobuf::stremio::core::runtime::{
 
 impl FromProtobuf<Action> for runtime::Action {
     fn from_protobuf(&self) -> Action {
-        match &self.action {
-            Some(runtime::action::Action::Ctx(action_ctx)) => match &action_ctx.args {
-                Some(action_ctx::Args::Authenticate(authenticate)) => {
-                    Action::Ctx(ActionCtx::Authenticate(authenticate.arg.from_protobuf()))
+        match &self.r#type {
+            Some(runtime::action::Type::Ctx(action_ctx)) => match &action_ctx.args {
+                Some(action_ctx::Args::Authenticate(auth_request)) => {
+                    Action::Ctx(ActionCtx::Authenticate(auth_request.from_protobuf()))
                 }
                 Some(action_ctx::Args::Logout(_args)) => Action::Ctx(ActionCtx::Logout),
-                Some(action_ctx::Args::UpdateSettings(update_settings)) => Action::Ctx(
-                    ActionCtx::UpdateSettings(update_settings.args.from_protobuf()),
-                ),
-                Some(action_ctx::Args::AddToLibrary(add_to_library)) => {
-                    Action::Ctx(ActionCtx::AddToLibrary(add_to_library.args.from_protobuf()))
+                Some(action_ctx::Args::UpdateSettings(settings)) => {
+                    Action::Ctx(ActionCtx::UpdateSettings(settings.from_protobuf()))
                 }
-                Some(action_ctx::Args::RemoveFromLibrary(remove_from_library)) => Action::Ctx(
-                    ActionCtx::RemoveFromLibrary(remove_from_library.args.to_owned()),
-                ),
-                Some(action_ctx::Args::RewindLibraryItem(rewind_library_item)) => Action::Ctx(
-                    ActionCtx::RewindLibraryItem(rewind_library_item.args.to_owned()),
-                ),
+                Some(action_ctx::Args::AddToLibrary(meta_item_preview)) => {
+                    Action::Ctx(ActionCtx::AddToLibrary(meta_item_preview.from_protobuf()))
+                }
+                Some(action_ctx::Args::RemoveFromLibrary(id)) => {
+                    Action::Ctx(ActionCtx::RemoveFromLibrary(id.to_owned()))
+                }
+                Some(action_ctx::Args::RewindLibraryItem(id)) => {
+                    Action::Ctx(ActionCtx::RewindLibraryItem(id.to_owned()))
+                }
                 Some(action_ctx::Args::PushUserToApi(_args)) => {
                     Action::Ctx(ActionCtx::PushUserToAPI)
                 }
@@ -48,40 +48,40 @@ impl FromProtobuf<Action> for runtime::Action {
                 }
                 None => unimplemented!("ActionCtx"),
             },
-            Some(runtime::action::Action::Link(action_link)) => match &action_link.args {
+            Some(runtime::action::Type::Link(action_link)) => match &action_link.args {
                 Some(action_link::Args::ReadData(_args)) => Action::Link(ActionLink::ReadData),
                 None => unimplemented!("ActionLink"),
             },
-            Some(runtime::action::Action::StreamingServer(action_streaming_server)) => {
+            Some(runtime::action::Type::StreamingServer(action_streaming_server)) => {
                 match &action_streaming_server.args {
                     Some(action_streaming_server::Args::Reload(_args)) => {
                         Action::StreamingServer(ActionStreamingServer::Reload)
                     }
-                    Some(action_streaming_server::Args::UpdateSettings(update_settings)) => {
+                    Some(action_streaming_server::Args::UpdateSettings(settings)) => {
                         Action::StreamingServer(ActionStreamingServer::UpdateSettings(
-                            update_settings.args.from_protobuf(),
+                            settings.from_protobuf(),
                         ))
                     }
                     None => unimplemented!("ActionLink"),
                 }
             }
-            Some(runtime::action::Action::Load(action_load)) => match &action_load.args {
-                Some(action_load::Args::CatalogsWithExtra(catalogs_with_extra)) => Action::Load(
-                    ActionLoad::CatalogsWithExtra(catalogs_with_extra.args.from_protobuf()),
-                ),
-                Some(action_load::Args::CatalogWithFilters(catalog_with_filters)) => Action::Load(
-                    ActionLoad::CatalogWithFilters(catalog_with_filters.args.from_protobuf()),
-                ),
-                Some(action_load::Args::LibraryWithFilters(library_with_filters)) => Action::Load(
-                    ActionLoad::LibraryWithFilters(library_with_filters.args.from_protobuf()),
-                ),
-                Some(action_load::Args::MetaDetails(meta_details)) => {
-                    Action::Load(ActionLoad::MetaDetails(meta_details.args.from_protobuf()))
+            Some(runtime::action::Type::Load(action_load)) => match &action_load.args {
+                Some(action_load::Args::CatalogsWithExtra(selected)) => {
+                    Action::Load(ActionLoad::CatalogsWithExtra(selected.from_protobuf()))
+                }
+                Some(action_load::Args::CatalogWithFilters(selected)) => {
+                    Action::Load(ActionLoad::CatalogWithFilters(selected.from_protobuf()))
+                }
+                Some(action_load::Args::LibraryWithFilters(selected)) => {
+                    Action::Load(ActionLoad::LibraryWithFilters(selected.from_protobuf()))
+                }
+                Some(action_load::Args::MetaDetails(selected)) => {
+                    Action::Load(ActionLoad::MetaDetails(selected.from_protobuf()))
                 }
                 Some(action_load::Args::Link(_args)) => Action::Load(ActionLoad::Link),
                 None => unimplemented!("ActionLink"),
             },
-            Some(runtime::action::Action::Unload(_args)) => Action::Unload,
+            Some(runtime::action::Type::Unload(_args)) => Action::Unload,
             None => unimplemented!("Action"),
         }
     }
