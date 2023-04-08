ea000b20299a0a05a4a1758e94bbfe5f7cb5e2de
Sleeyax <yourd3veloper@gmail.com>
2023-04-08 17:30:01 +0200

    make core wrapper platform independent

---
+++ src/commonMain/kotlin/com/stremio/core/Core.kt
@@ -0,0 +1,20 @@
+package com.stremio.core
+
+import com.stremio.core.runtime.EnvError
+import com.stremio.core.runtime.RuntimeEvent
+import com.stremio.core.runtime.msg.Action
+import com.stremio.core.types.resource.Stream
+import pbandk.Message
+
+interface EventListener {
+    fun onEvent(event: RuntimeEvent)
+}
+
+expect object Core {
+    fun addEventListener(listener: EventListener)
+    fun removeEventListener(listener: EventListener)
+    fun initialize(storage: Storage): EnvError?
+    fun dispatch(action: Action, field: Field?)
+    fun decodeStreamData(streamData: String): Stream?
+    inline fun <reified T : Message> getState(field: Field): T
+}
