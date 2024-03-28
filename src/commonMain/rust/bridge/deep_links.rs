b5cff01004c2ffba009402a6e1dbb77a5a02217b
Tim <tymmesyde@gmail.com>
2024-03-28 13:31:48 +0100

    refactor: rename deepLinks package to deeplinks

--- src/commonMain/rust/bridge/deep_links.rs
+++ src/commonMain/rust/bridge/deep_links.rs
@@ -1,20 +1,20 @@
 use stremio_core::deep_links::{DiscoverDeepLinks, LibraryDeepLinks};
 
-use crate::protobuf::stremio::core::deep_links;
+use crate::protobuf::stremio::core::deeplinks;
 
 use super::ToProtobuf;
 
-impl ToProtobuf<deep_links::DiscoverDeepLinks, ()> for DiscoverDeepLinks {
-    fn to_protobuf(&self, _args: &()) -> deep_links::DiscoverDeepLinks {
-        deep_links::DiscoverDeepLinks {
+impl ToProtobuf<deeplinks::DiscoverDeepLinks, ()> for DiscoverDeepLinks {
+    fn to_protobuf(&self, _args: &()) -> deeplinks::DiscoverDeepLinks {
+        deeplinks::DiscoverDeepLinks {
             discover: self.discover.clone(),
         }
     }
 }
 
-impl ToProtobuf<deep_links::LibraryDeepLinks, ()> for LibraryDeepLinks {
-    fn to_protobuf(&self, _args: &()) -> deep_links::LibraryDeepLinks {
-        deep_links::LibraryDeepLinks {
+impl ToProtobuf<deeplinks::LibraryDeepLinks, ()> for LibraryDeepLinks {
+    fn to_protobuf(&self, _args: &()) -> deeplinks::LibraryDeepLinks {
+        deeplinks::LibraryDeepLinks {
             library: self.library.clone(),
         }
     }
