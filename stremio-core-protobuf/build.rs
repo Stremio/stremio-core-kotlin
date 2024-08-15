34bab4c6a93ccd899717db05e273d877e5fb1951
Lachezar Lechev <elpiel93@gmail.com>
2024-08-15 10:28:31 +0300

    fix(stremio-core-protobuf): build script creates protobuf folder if it's missing
    Signed-off-by: Lachezar Lechev <elpiel93@gmail.com>

--- stremio-core-protobuf/build.rs
+++ stremio-core-protobuf/build.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{fs::DirBuilder, path::PathBuf};
 
 use glob::glob;
 use prost_build::Config;
@@ -24,9 +24,19 @@ fn main() {
     }
     let file_descriptors =
         protox::compile(proto_paths, [proto_dir]).expect("Expected file descriptors");
+
+    let protobuf_gen_dir = PathBuf::from(CARGO_MANIFEST_DIR)
+        .join("src")
+        .join("protobuf");
+    // create protobuf folder if it doesn't exist
+    DirBuilder::new()
+        .recursive(true)
+        .create(protobuf_gen_dir.clone())
+        .expect("Should create src/protobuf dir if it does not exist.");
+
     Config::new()
         .compile_well_known_types()
-        .out_dir("src/protobuf")
+        .out_dir(&protobuf_gen_dir.display().to_string())
         .include_file("mod.rs")
         .compile_fds(file_descriptors)
         .expect("Expected successful protobuf codegen");
