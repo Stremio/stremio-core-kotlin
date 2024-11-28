b330179b56c89696cc439432b276ce2cddeaca02
Tim <tymmesyde@gmail.com>
2024-11-28 18:25:18 +0100

    feat: add trakt field to user profile

--- stremio-core-protobuf/src/bridge/date.rs
+++ stremio-core-protobuf/src/bridge/date.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{DateTime, TimeDelta, TimeZone, Utc};
 
 use crate::bridge::{FromProtobuf, ToProtobuf};
 use crate::protobuf::google::protobuf::Timestamp;
@@ -14,6 +14,15 @@ impl ToProtobuf<Timestamp, ()> for DateTime<Utc> {
         Timestamp {
             seconds: self.timestamp(),
             nanos: self.timestamp_subsec_nanos() as i32,
+        }
+    }
+}
+
+impl ToProtobuf<Timestamp, ()> for TimeDelta {
+    fn to_protobuf<E: stremio_core::runtime::Env + 'static>(&self, _args: &()) -> Timestamp {
+        Timestamp {
+            seconds: self.num_seconds(),
+            nanos: self.num_nanoseconds().map_or(0, |nanos| nanos as i32),
         }
     }
 }
